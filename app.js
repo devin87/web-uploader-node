1c12fe22528cf780a7886251aeb9e05df36343ad
Devin <devin@morequick.net>
2014-09-23 15:25:49 +0800

    初次提交
    Signed-off-by: Devin <devin@morequick.net>

---
+++ app.js
@@ -0,0 +1,37 @@
+//处理文件上传页面
+var http = require('http'),
+    express = require('express'),
+
+    Q = require('./lib/Q.js'),
+
+    app = express();
+
+app.use(express.static('./www'));
+app.use(express.bodyParser({ uploadDir: './upload', limit: '500mb' }));
+
+app.get('/', function (req, res) {
+    res.redirect("/demo/default.html");
+});
+
+app.post('/upload', function (req, res) {
+    Object.forEach(req.files, function (k, f) {
+        var path = f.path,
+            fileName = f.name;
+
+        console.log(fileName + " => " + path + "     | " + Q.formatSize(f.size).text);
+    });
+
+    var user = req.body["user"],
+        name = req.body["name"];
+
+    res.send(JSON.stringify({ time: Date.now(), user: user, name: name }));
+});
+
+var server = http.createServer(app);
+
+server.listen(3000, function () {
+    var addr = server.address();
+    console.log();
+    console.log("server running at " + addr.address + ":" + addr.port);
+    console.log();
+});
