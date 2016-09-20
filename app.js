781004def386fe97229e7056b7ef2244943ec636
Devin <devin87@qq.com>
2016-09-20 09:48:51 +0800

    1. 同步web-uploader代码，添加秒传、分片上传、断点续传功能 2. 使用 express 4.x 改写服务端代码 3. 移除 node_modules 目录，使用时请执行 install.bat 安装依赖
    Signed-off-by: Devin <devin87@qq.com>

--- app.js
+++ app.js
@@ -1,37 +1,83 @@
 //处理文件上传页面
-var http = require('http'),
+var fs = require('fs'),
+
     express = require('express'),
+    formidable = require("formidable"),
 
     Q = require('./lib/Q.js'),
-
     app = express();
 
 app.use(express.static('./www'));
-app.use(express.bodyParser({ uploadDir: './upload', limit: '4000mb' }));
 
 app.get('/', function (req, res) {
     res.redirect("/demo/default.html");
 });
 
-app.post('/upload', function (req, res) {
-    Object.forEach(req.files, function (k, f) {
-        var path = f.path,
-            fileName = req.body["fileName"] || f.name;
+function process_upload_file(req, res, callback) {
+    var form = new formidable.IncomingForm();
 
-        console.log(fileName + " => " + path + "     | " + Q.formatSize(f.size));
+    form.uploadDir = "upload";
+    form.maxFieldsSize = 2 * 1024 * 1024 * 1024;  //2G
+
+    form.parse(req, function (err, fields, files) {
+        var upfile;
+
+        Object.forEach(files, function (key, file) {
+            upfile = file;
+        });
+
+        callback && callback(form, upfile, fields);
     });
+}
 
-    var user = req.body["user"],
-        name = req.body["name"];
+function finish_upload(req, res, fields) {
+    var result = {
+        time: Date.now(),
+        type: req.query["type"],
+        user: fields["user"],
+        name: fields["name"]
+    };
 
-    res.send(JSON.stringify({ time: Date.now(), user: user, name: name }));
+    res.send(JSON.stringify(result));
+}
+
+app.all('/upload', function (req, res) {
+    var action = req.query["action"],
+        hash = req.query["hash"];
+
+    if (!hash) {
+        process_upload_file(req, res, function (form, file, fields) {
+            var fileName = fields["fileName"];
+            fs.rename(file.path, form.uploadDir + "/" + (fileName || file.name));
+
+            finish_upload(req, res, fields);
+        });
+    } else {
+        var path = "upload/" + hash,
+            path_ok = path + ".ok";
+
+        if (action == "query") {
+            if (fs.existsSync(path_ok)) res.send('ok');
+            else if (fs.existsSync(path)) res.send(fs.statSync(path).size + "");
+            else res.send("0");
+        } else {
+            process_upload_file(req, res, function (form, file, fields) {
+                fs.appendFileSync(path, fs.readFileSync(file.path));
+                fs.unlink(file.path);
+
+                var isOk = req.query["ok"] == "1";
+                if (!isOk) {
+                    res.send("1");
+                } else {
+                    fs.rename(path, path_ok);
+                    finish_upload(req, res, fields);
+                }
+            });
+        }
+    }
 });
 
-var server = http.createServer(app);
-
-server.listen(3000, function () {
-    var addr = server.address();
-    console.log();
-    console.log("server running at " + addr.address + ":" + addr.port);
+app.listen(3000, function () {
+    console.log("server running at 127.0.0.1:3000");
     console.log();
 });
--- www/js/Q.Uploader.js
+++ www/js/Q.Uploader.js
@@ -1,9 +1,10 @@
 ﻿/// <reference path="Q.js" />
+/// <reference path="Q.md5File.js" />
 /*
 * Q.Uploader.js 文件上传管理器 1.0
 * https://github.com/devin87/web-uploader
 * author:devin87@qq.com  
-* update:2016/04/22 14:58
+* update:2016/09/20 09:29
 */
 (function (window, undefined) {
     "use strict";
@@ -22,6 +23,8 @@
 
         setOpacity = Q.setOpacity,
         getOffset = Q.getOffset,
+
+        md5File = Q.md5File,
 
         E = Q.event,
         addEvent = E.add,
@@ -109,23 +112,25 @@
     function set_task_speed(task, total, loaded) {
         if (!total || total <= 0) return;
 
-        var timeNow = Date.now(), tick;
+        var nowTime = Date.now(), tick;
 
         //上传完毕,计算平均速度(Byte/s)
         if (loaded >= total) {
-            tick = timeNow - task.timeStart;
+            tick = nowTime - task.startTime;
             if (tick) task.avgSpeed = Math.min(Math.round(total * 1000 / tick), total);
+            else if (!task.speed) task.avgSpeed = task.speed = total;
 
-            task.timeEnd = timeNow;
+            task.time = tick || 0;
+            task.endTime = nowTime;
             return;
         }
 
         //即时速度(Byte/s)
-        tick = timeNow - task.time;
+        tick = nowTime - task.lastTime;
         if (tick < 200) return;
 
         task.speed = Math.min(Math.round((loaded - task.loaded) * 1000 / tick), task.total);
-        task.time = timeNow;
+        task.lastTime = nowTime;
     }
 
     /*
@@ -152,6 +157,12 @@
 
             allows: "",        //允许上传的文件类型(扩展名),多个之间用逗号隔开
             disallows: "",     //禁止上传的文件类型(扩展名)
+
+            isSlice: false,               //是否启用分片上传，若为true，则isQueryState和isMd5默认为true
+            chunkSize: 2 * 1024 * 1024,   //默认分片大小为2MB
+            isQueryState:false,           //是否查询文件状态（for 秒传或续传）
+            isMd5: false,                 //是否计算上传文件md5值
+            isUploadAfterHash:true,       //是否在Hash计算完毕后再上传
 
             container:element, //一般无需指定
             getPos:function,   //一般无需指定
@@ -215,6 +226,12 @@
 
         self.allows = split_to_map(ops.allows);         //允许上传的文件类型（扩展名）,多个之间用逗号隔开 eg:.jpg,.png
         self.disallows = split_to_map(ops.disallows);   //禁止上传的文件类型（扩展名）,多个之间用逗号隔开
+
+        self.chunkSize = ops.chunkSize || 2 * 1024 * 1024;            //分片上传大小
+        self.isSlice = !!ops.isSlice;                                 //是否启用分片上传
+        self.isQueryState = !!def(ops.isQueryState, self.isSlice);    //是否查询文件状态（for 秒传或续传）
+        self.isMd5 = !!def(ops.isMd5, self.isSlice);                  //是否计算上传文件md5值
+        self.isUploadAfterHash = ops.isUploadAfterHash !== false;     //是否在Hash计算完毕后再上传
 
         //ie9及以下不支持click触发(即使能弹出文件选择框,也无法获取文件数据,报拒绝访问错误)
         //若上传按钮位置不确定(比如在滚动区域内),则无法触发文件选择
@@ -388,7 +405,7 @@
         },
 
         //添加一个上传任务
-        addTask: function (input, file, arg) {
+        addTask: function (input, file) {
             if (!input && !file) return;
 
             var name, size;
@@ -415,8 +432,6 @@
 
                 input: input,
                 file: file,
-
-                arg: arg,
 
                 state: isSkip ? UPLOAD_STATE_SKIP : UPLOAD_STATE_READY
             };
@@ -469,7 +484,8 @@
         },
 
         //取消上传任务
-        cancel: function (taskId) {
+        //onlyCancel: 若为true,则仅取消上传而不触发任务完成事件
+        cancel: function (taskId, onlyCancel) {
             var self = this,
                 task = self.get(taskId);
 
@@ -494,7 +510,7 @@
                 self.iframe.contentWindow.location = "about:blank";
             }
 
-            return self.complete(task, UPLOAD_STATE_CANCEL);
+            return onlyCancel ? self : self.complete(task, UPLOAD_STATE_CANCEL);
         },
 
         //移除任务
@@ -550,10 +566,99 @@
             self.fire("upload", task, function (result) {
                 if (result === false) return self.complete(task, UPLOAD_STATE_SKIP);
 
-                if (self.html5 && task.file) self._upload_html5(task);
+                if (self.html5 && task.file) self._upload_html5_ready(task);
                 else if (task.input) self._upload_html4(task);
                 else self.complete(task, UPLOAD_STATE_SKIP);
             });
+
+            return self;
+        },
+
+        //根据 task.hash 查询任务状态（for 秒传或续传）
+        queryState: function (task, callback) {
+            var self = this,
+                url = self.url,
+                xhr = new XMLHttpRequest();
+
+            url += (url.indexOf("?") == -1 ? "?" : "&") + "action=query&hash=" + (task.hash || task.name);
+
+            xhr.open("GET", url);
+            xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState != 4) return;
+
+                var result;
+
+                if (xhr.status >= 200 && xhr.status < 400) {
+                    var result = xhr.responseText;
+                    if (result === "ok") {
+                        task.queryOK = true;
+                        self.cancel(task.id, true).complete(task, UPLOAD_STATE_COMPLETE);
+                    } else {
+                        var start = !result || isNaN(result) ? 0 : +result;
+                        if (start != Math.floor(start)) start = 0;
+
+                        task.sliceStart = start;
+                    }
+                }
+
+                fire(callback, self, xhr, result);
+            };
+
+            xhr.onerror = function () {
+                fire(callback, self, xhr);
+            };
+
+            xhr.send(null);
+
+            return self;
+        },
+
+        //处理html5上传（包括秒传和断点续传）
+        _upload_html5_ready: function (task) {
+            var self = this;
+
+            //上传处理
+            var goto_upload = function () {
+                if (task.state == UPLOAD_STATE_COMPLETE) return;
+
+                if (self.isSlice) self._upload_slice(task);
+                else self._upload_html5(task);
+            };
+
+            var after_hash = function (callback) {
+                //自定义hash事件
+                self.fire("hash", task, function () {
+                    if (task.hash && self.isQueryState && task.state != UPLOAD_STATE_COMPLETE) self.queryState(task, callback);
+                    else callback();
+                });
+            };
+
+            //计算文件hash
+            var compute_hash = function (callback) {
+                //计算上传文件md5值
+                if (self.isMd5 && md5File) {
+                    var hashProgress = self.fns.hashProgress;
+
+                    md5File(task.file, function (md5, time) {
+                        task.hash = md5;
+                        task.timeHash = time;
+                        after_hash(callback);
+                    }, function (pvg) {
+                        fire(hashProgress, self, task, pvg);
+                    });
+                } else {
+                    after_hash(callback);
+                }
+            };
+
+            if (self.isUploadAfterHash) {
+                compute_hash(goto_upload);
+            } else {
+                goto_upload();
+                compute_hash();
+            }
 
             return self;
         },
@@ -599,7 +704,8 @@
 
             xhr.open("POST", task.url);
 
-            xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+            //移除自定义标头,以防止跨域上传被拦截
+            //xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
 
             self.fire("send", task, function (result) {
                 if (result === false) return self.complete(task, UPLOAD_STATE_SKIP);
@@ -643,7 +749,7 @@
 
         //已开始发送数据
         _afterSend: function (task) {
-            task.time = task.timeStart = Date.now();
+            task.lastTime = task.startTime = Date.now();
 
             task.state = UPLOAD_STATE_PROCESSING;
             this._lastTask = task;
@@ -691,7 +797,7 @@
             if (task) {
                 if (state != undefined) task.state = state;
 
-                if (task.state == UPLOAD_STATE_PROCESSING) {
+                if (task.state == UPLOAD_STATE_PROCESSING || state == UPLOAD_STATE_COMPLETE) {
                     task.state = UPLOAD_STATE_COMPLETE;
                     self.progress(task, task.size, task.size);
                 }
